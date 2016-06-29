6ce4552091ba3b86e915e9d999e8d19572f0cb58
wyvern.wu <wyvern.wu@pingpp.com>
2016-06-29 10:31:08 +0800

    add search

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,18 +1,25 @@
-System.register('pingxx-search/main', ['flarum/extend', 'flarum/app'], function (_export) {
-  'use strict';
+'use strict';
 
-  var extend, app;
-  return {
-    setters: [function (_flarumExtend) {
-      extend = _flarumExtend.extend;
-    }, function (_flarumApp) {
-      app = _flarumApp['default'];
-    }],
-    execute: function () {
+System.register('pingxx-search/main', ['flarum/extend', 'flarum/app', 'flarum/components/Search'], function (_export, _context) {
+    "use strict";
 
-      app.initializers.add('pingxx-search', function () {
-        // TODO
-      });
-    }
-  };
+    var override, app, Search;
+    return {
+        setters: [function (_flarumExtend) {
+            override = _flarumExtend.override;
+        }, function (_flarumApp) {
+            app = _flarumApp.default;
+        }, function (_flarumComponentsSearch) {
+            Search = _flarumComponentsSearch.default;
+        }],
+        execute: function () {
+
+            app.initializers.add('pingxx-search', function () {
+                console.log("aaaaa");
+                override(Search.prototype, 'sourceItems', function (original) {
+                    return original().remove('users');
+                });
+            });
+        }
+    };
 });
--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,6 +1,10 @@
-import { extend } from 'flarum/extend';
+import { override } from 'flarum/extend';
 import app from 'flarum/app';
+import Search from 'flarum/components/Search';
 
 app.initializers.add('pingxx-search', () => {
-  // TODO
+    console.log("aaaaa")
+    override(Search.prototype, 'sourceItems', function(original) {
+        return original().remove('users');
+    });
 });
